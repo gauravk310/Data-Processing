6ef91fabb3c8bf39fb0cd61e98de41d75aabd7e8
iamsorry0310 <gskadam3b@gmail.com>
2024-07-16 12:00:43 +0530

    imporvement

--- app.py
+++ app.py
@@ -448,6 +448,13 @@ def encoding():
                 
 def about():
     st.title("About Us")
+    st.header("Improvment")
+    st.markdown("""
+1. Design
+2.Multiple Methos for Outlier Detection
+3.Multiple Methos for Encoding
+4. More Graph plot options
+""")
                     
 
 
